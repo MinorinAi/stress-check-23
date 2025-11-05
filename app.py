73e9ed74a7d56c700bf5393f56e67a250672c304
MINORI NAGATO <yipyipjoseph@gmail.com>
2025-11-05 13:20:29 +0900

    Fix import statement for japanize_matplotlib

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 import matplotlib
-import japanize-matplotlib
+import japanize_matplotlib
 from datetime import datetime
 import uuid
 from reportlab.lib.pagesizes import A4
